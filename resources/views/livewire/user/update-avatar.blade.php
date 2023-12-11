ade49a304284a885840e7f42f81d11094b3483c2
Michael T <66504185+michaeltukdev@users.noreply.github.com>
2023-12-11 13:19:42 +0000

    Fixing responsiveness for settings
    I totally didn't forget to make it responsive.

--- resources/views/livewire/user/update-avatar.blade.php
+++ resources/views/livewire/user/update-avatar.blade.php
@@ -1,12 +1,12 @@
 <form wire:submit="save">
     <div class="post__container">
-        <div class="avatar-section d-flex">
-            <div>
+        <div class="avatar-section d-flex flex-wrap flex-md-nowrap">
+            <div class="m-auto m-md-none">
                 <img width="150" height="150" class="br__8" alt="User profile picture" src="{{ Auth()->user()->avatar }}">
                 <p class="current__avatar text-center mt-2">Current Avatar</p>
             </div>
     
-            <div class="upload__avatar text-center">
+            <div class="upload__avatar text-center my-4 my-lg-0 py-5 py-md-none">
                 <input type="file" id="avatar" wire:model="avatar" accept="image/*" hidden />
                 <label for="avatar" class="avatar__label">
                     <h5>Click to select new avatar</h5>
@@ -15,7 +15,7 @@
             </div>
             
     
-            <div>
+            <div class="m-auto m-md-none">
                 <img width="150" height="150" class="new__avatar br__8" alt="User profile picture" src="@if($avatar) {{ $avatar->temporaryUrl() }} @else {{asset('/images/questionMark.png')}} @endif">
                 <p class="current__avatar text-center mt-2">New Avatar</p>
             </div>
